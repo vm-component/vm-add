2af5b30842a934c5c7c4433d3e185f624e776869
向松涛 <280304286@163.com>
2017-09-17 18:50:30 +0800

    chore(release): 7.1.0

---
+++ tools/git-release.ts
@@ -0,0 +1,18 @@
+const path = require("path")
+const {exec, echo} = require("shelljs")
+const {readFileSync} = require("fs")
+const pkg = JSON.parse(
+  readFileSync(path.resolve(__dirname, "..", "package.json"))
+)
+
+let version = pkg.version
+
+echo("Push new version to Github...")
+exec("git add .")
+// exec('git config user.name "xiangsongtao"')
+// exec('git config user.email "280304286@163.com"')
+exec(`git commit -m "chore(release): ${version}"`)
+exec(
+  `git push --force --quiet`
+)
+echo("Push Done!!")
