ca37f3da064f3a37e64677984d87bff4b9600c8f
xiangsongtao <280304286@163.com>
2017-09-17 21:37:03 +0800

    chore(release): 7.1.0

--- tools/gh-pages-publish.ts
+++ tools/gh-pages-publish.ts
@@ -28,4 +28,4 @@ exec('git commit -m "docs(docs): update gh-pages"')
 exec(
   `git push --force --quiet "https://${ghToken}@${repository}" master:gh-pages`
 )
-echo("Docs deployed!!")
+echo("Docs deployed!!");
