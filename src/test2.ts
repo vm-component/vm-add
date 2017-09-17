5691aefd9359c5ec0479c6292179e6c7e08b42f5
向松涛 <280304286@163.com>
2017-09-17 09:13:28 +0800

    feat: 123

--- src/test2.ts
+++ src/test2.ts
@@ -1,10 +1,11 @@
 export default {
-  a: 'dev-cz',
-  a2: 'dev-cz',
-  a3: 'dev-cz',
-  a4: 'dev-cz',
-  a5: 'dev-cz',
-  a6: 'dev-cz',
-  a7: 'dev-cz',
-  a8: 'dev-cz',
+  a: "dev-cz",
+  a2: "dev-cz",
+  a3: "dev-cz",
+  a4: "dev-cz",
+  a5: "dev-cz",
+  a6: "dev-cz",
+  a7: "dev-cz",
+  a71: "dev-cz",
+  a8: "dev-cz"
 }
