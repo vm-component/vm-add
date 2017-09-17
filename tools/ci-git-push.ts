0450bb5fc3e0485b974c0a2000bd8b77185d17df
xiangsongtao <280304286@163.com>
2017-09-17 21:28:33 +0800

    chore(release): 7.1.0

--- tools/ci-git-push.ts
+++ tools/ci-git-push.ts
@@ -1,9 +1,23 @@
 const path = require("path")
 const {exec, echo} = require("shelljs")
 const {readFileSync} = require("fs")
-const pkg = JSON.parse(
-  readFileSync(path.resolve(__dirname, "..", "package.json"))
-)
+const url = require("url")
+
+let repoUrl
+let pkg = JSON.parse(readFileSync("package.json") as any)
+if (typeof pkg.repository === "object") {
+  if (!pkg.repository.hasOwnProperty("url")) {
+    throw new Error("URL does not exist in repository section")
+  }
+  repoUrl = pkg.repository.url
+} else {
+  repoUrl = pkg.repository
+}
+
+let parsedUrl = url.parse(repoUrl)
+let repository = (parsedUrl.host || "") + (parsedUrl.path || "")
+let ghToken = 'c38f9ab9280a7dee1b5a32e21bba949e282fce21'
+// let ghToken = process.env.GH_TOKEN
 
 let version = pkg.version
 
@@ -13,6 +27,6 @@ exec('git config user.name "xiangsongtao"')
 exec('git config user.email "280304286@163.com"')
 exec(`git commit -m "chore(release): ${version}"`)
 exec(
-  `git push origin master`
+  `git push --force --quiet "https://${ghToken}@${repository}" master`
 )
 echo("Push Done!!")
